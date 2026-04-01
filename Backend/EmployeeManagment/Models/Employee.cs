8200047b68c2a1547c2163dc61e0127219231b63
Dizuw <kamilwawrzayniak1025@gmail.com>
2026-04-02 01:20:52 +0200

    Add project files.

---
+++ Backend/EmployeeManagment/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagment.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Position { get; set; }
+    }
+}
