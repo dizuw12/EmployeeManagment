faee908a298ce44266b25191e2593e8ed4b2d417
Dizuw <kamilwawrzayniak1025@gmail.com>
2026-04-02 01:49:49 +0200

    Installing Enitiy Framerwork, Adding DbContext, Configuring Db, Adding Cors Policy

---
+++ Backend/EmployeeManagment/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagment.Models;
+using Microsoft.EntityFrameworkCore;
+namespace EmployeeManagment.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
+    }
+}
