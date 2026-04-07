f49ceb0dc94756b06e65e101747d2509199c552e
Dizuw <kamilwawrzayniak1025@gmail.com>
2026-04-07 21:58:26 +0200

    Creating repository interface for Employee, Creating Employee repository class and adding logic for Adding Getting Updeating and Deleting Employees

---
+++ Backend/EmployeeManagment/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagment.Models;
+
+namespace EmployeeManagment.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+        Task AddEmployeeAsync(Employee employee);
+        Task UpdateEmployeeAsync(Employee employee);
+        Task DeleteEmployeeAsync(int id);
+
+    }
+}
