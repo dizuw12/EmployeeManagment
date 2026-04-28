5713bda7ac6da6d441b40293fa8cdc6afa6fdbcd
Dizuw <kamilwawrzayniak1025@gmail.com>
2026-04-28 23:33:16 +0200

    Creating Frontend with angular18, creating employee service, creating table for showing employee data, defining routes and creating simple navigation menu, creating employee form to crating employee extending the service to send a http post request and handeling errors, showing error messages

---
+++ Frontend/employee-managment-app/src/app/employee-form/employee-form.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-employee-form',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './employee-form.component.html',
+  styleUrl: './employee-form.component.css'
+})
+export class EmployeeFormComponent {
+
+  employee: Employee = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    city: '',
+    position: ''
+  }
+  errorMessage : string = "";
+  
+
+  constructor(private employeeService: EmployeeService, private router: Router){}
+  onSubmit() : void{
+    this.employeeService.createEmployee(this.employee)
+    .subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) =>{
+        console.error(err);
+        this.errorMessage = `Error occoured: ${err.status}`;
+      }
+    });
+    
+  }
+
+}
