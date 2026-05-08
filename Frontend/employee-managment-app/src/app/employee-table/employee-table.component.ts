d638eae5dcbd6d93a5d40ed38ec7265d03cd2f6d
Dizuw <kamilwawrzayniak1025@gmail.com>
2026-05-08 13:08:07 +0200

    Adding Deleting employee button and functionality also preparation for editing employee

--- Frontend/employee-managment-app/src/app/employee-table/employee-table.component.ts
+++ Frontend/employee-managment-app/src/app/employee-table/employee-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-table',
@@ -14,7 +15,7 @@ export class EmployeeTableComponent {
   
   employees: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService){}
+  constructor(private employeeService: EmployeeService, private router: Router){}
   
   ngOnInit(){
     this.employeeService.getEmployees().subscribe((data: Employee[]) => {
@@ -23,5 +24,18 @@ export class EmployeeTableComponent {
     });
   }  
   
+  deleteEmployee(id: number) : void{
+    this.employeeService.deleteEmployee(id).subscribe({
+      next: (response) => {
+        this.employees = this.employees.filter(e => e.id !== id);
+      },
+      error: (err) => {
+        console.error('Error deleting employee',err);
+      }
+    })
+  }
 
+  editEmployee(id: number) : void{
+    this.router.navigate(['/edit', id]);
+  }
 }
